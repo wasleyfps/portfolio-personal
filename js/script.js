2b817600e8978b28d4c07571f5caf0e86b49e9e5
Wasley <88601440+wasleyfps@users.noreply.github.com>
2023-08-29 14:30:47 -0300

    update portfolio

--- js/script.js
+++ js/script.js
@@ -37,6 +37,24 @@ window.onscroll = () => {
 
 
 /*==================== scroll reveal ====================*/
+    ScrollReveal({ 
+        reset: true
+        distance: '80px',
+        duration: 2000, 
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content, .home', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 /*==================== typed js ====================*/
+    const typed = new typed('.multiple-text', {
+        strings: ['Desenvolvedor front-end', 'Youtuber', 'Editor de Vídeo'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
